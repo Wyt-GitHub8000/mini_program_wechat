b96c6fb7ea57607770bc27c7100b490af083f4e8
Wyt-Git <415096996@qq.com>
2020-12-09 23:22:31 +0800

    小程序登录流程

--- app.js
+++ app.js
@@ -1,9 +1,54 @@
+const TOKEN = 'token'
 //注册一个小程序示例
 App({
+  globalData:{
+    token: ''
+  },
   //小程序初始化完成后(app.json加载解析完毕)会执行的生命周期函数
   onLaunch:function(){
-    //通常在该生命周期函数中获取用户信息
-    
+    //验证本地缓存的token是否过期
+    const token = wx.getStorageSync('TOKEN')
+    if(token && token.length>0){
+      //有token,服务器验证token是否过期
+      wx.request({
+        url: 'http://123.207.32.32:3000/auth',
+        method: 'POST',
+        header: {
+          token
+        },
+        success: (res)=>{
+          //根据服务器返回的错误码判断是否登录过期，登录过期则重新登录。未过期则进行相关请求
+        },
+        fail: (err)=>{
+          console.log(err);
+        }
+      })
+    }else{
+       //没有token，用户第一次登录。开始登录操作
+      wx.login({
+        success: (res)=>{
+          //1.获取code
+          const code = res.code;
+          //2.将请求发送给公司服务器
+          wx.request({
+            url: 'http://123.207.32.32:3000/login',
+            method: 'post',
+            data: {
+              code
+            },
+            success: (res)=>{
+              console.log(res)
+              const token = res.data.token;
+              //存储到globalData对象中。缺点是小程序关闭token也随之删除
+              this.globalData.token = token;
+  
+              //进行本地存储
+              wx.setStorage(TOKEN,token)
+            }
+          })
+        }
+      })
+    }
   },
   //小程序界面显示出来后执行该生命周期函数
   onShow:function(){
